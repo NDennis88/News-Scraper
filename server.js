5d49135fd3c9980910085050461c142026fed8d5
Nai-Muh Dennis <naimuhdennis@gmail.com>
2019-04-25 18:11:21 -0700

    working scraping title, link and summary

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ axios.get("https://www.npr.org/sections/news/").then(function(response) {
     // An empty array to save the scraped data
     var results = [];
     // With cheerio, find each h2-tag with the "title" class
-    $(h2.title).each(function(i, element) {
+    $("h2.title").each(function(i, element) {
         // Save the text of the element in a "title" variable
         var title = $(element).text();
         // Save the values of any "href" attributes
@@ -26,11 +26,11 @@ axios.get("https://www.npr.org/sections/news/").then(function(response) {
     });
 
     // With cheerio, find each p-tag with the "teaser" class
-    $(p.teaser).each(function(i, element) {
+    $("p.teaser").each(function(i, element) {
         var summary = $(element).text();
         // Save the results in an object that we'll push into the results array
         results.push({
-            summary: teaser
+            teaser: summary
         });
     });
 
