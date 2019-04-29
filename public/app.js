39d28a7af5b07396fdaa010f2f66ad8e0eb1ca0f
Nai-Muh Dennis <naimuhdennis@gmail.com>
2019-04-28 20:41:44 -0700

    updated notes section in app.js

--- public/app.js
+++ public/app.js
@@ -41,7 +41,8 @@ $(document).on("click", ".btn-fetch", function() {
   
         $(".modal-title").append("<h5>" + data.title + "</h5>");
         $(".input").append("<textarea id='bodyinput' name='body'></textarea>");
-        $(".input").append("<button data-id='" + data._id + "' id='savenote' class='btn btn-primary btn-sm' style='margin-top:20px;'data-dismiss='modal'>Save Note</button>");
+        $(".input").append("<button data-id='" + data._id + "' id='savenote' class='btn btn-primary btn-sm' style='margin-top:20px';'data-dismiss='modal'>Save Note</button>");
+        $(".input").append("<button data-id='" + data._id + "' id='deleteenote' class='btn btn-primary btn-sm' style='margin-top:20px;'data-dismiss='modal'>Delete Note</button>");
   
         // If there's a note in the article
         if (data.note) {
@@ -80,6 +81,32 @@ $(document).on("click", ".btn-fetch", function() {
     $("#bodyinput").val("");
   });
   
+    // When you click the Delete Note button
+    $(document).on("click", "#deletenote", function() {
+      // Grab the id associated with the article from the submit button
+      var thisId = $(this).attr("data-id");
+      console.log(thisId);
+    
+      // Run a DELETE request to change the note, using what's entered in the inputs
+      $.ajax({
+        method: "DELETE",
+        url: "/articles/" + thisId,
+        data: {
+          // Value taken from note textarea
+          body: $("#bodyinput").val()
+        }
+      })
+      
+        .done(function(data) {
+          // Log the response
+          console.log(data);
+          // Empty the notes section
+          // $("#bodyinput").empty();
+        });
+    
+      // Remove the values entered in the input and textarea for note entry
+      $("#bodyinput").val("");
+    });
   
   
   // When you click the Save Article button
@@ -100,55 +127,3 @@ $(document).on("click", ".btn-fetch", function() {
   });
   
   
-
-
-//       // With that done, add the note information to the page
-//       .then(function(data) {
-//         console.log(data);
-//         // The title of the article
-//         $("#notes").append("<h2>" + data.title + "</h2>");
-//         // An input to enter a new title
-//         $("#notes").append("<input id='titleinput' name='title' >");
-//         // A textarea to add a new note body
-//         $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-//         // A button to submit a new note, with the id of the article saved to it
-//         $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
-//         // If there's a note in the article
-//         if (data.note) {
-//           // Place the title of the note in the title input
-//           $("#titleinput").val(data.note.title);
-//           // Place the body of the note in the body textarea
-//           $("#bodyinput").val(data.note.body);
-//         }
-//       });
-//   });
-  
-//   // When you click the savenote button
-//     $(document).on("click", "#savenote", function() {
-//     // Grab the id associated with the article from the submit button
-//     var thisId = $(this).attr("data-id");
-  
-//     // Run a POST request to change the note, using what's entered in the inputs
-//     $.ajax({
-//       method: "POST",
-//       url: "/articles/" + thisId,
-//       data: {
-//         // Value taken from title input
-//         title: $("#titleinput").val(),
-//         // Value taken from note textarea
-//         body: $("#bodyinput").val()
-//       }
-//     })
-//       // With that done
-//       .then(function(data) {
-//         // Log the response
-//         console.log(data);
-//         // Empty the notes section
-//         $("#notes").empty();
-//       });
-  
-//     // Also, remove the values entered in the input and textarea for note entry
-//     $("#titleinput").val("");
-//     $("#bodyinput").val("");
-//   });
